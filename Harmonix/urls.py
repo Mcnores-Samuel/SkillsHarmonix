c3936d6df09e5e335ccc4d222b393faa2d4e8093
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-11-27 21:16:42 +0200

    created endpoints for the job post creation form

--- Harmonix/urls.py
+++ Harmonix/urls.py
@@ -1,14 +1,24 @@
+"""This module contains the urls for all urls in the Harmonix app.
+These urls are used to navigate to the different pages in the Harmonix app.
+"""
 from django.urls import path
-from .views.registers import (sign_up, log_in, create_business_profile,
-                              create_professional_profile)
+from .views.registers import (sign_up, log_in, log_out,
+                              create_business_profile,
+                              create_professional_profile,
+                              create_job_post)
+
 
 urlpatterns = [
+    # Paths for the registers in the Harmonix app.
     path('sign_up/', sign_up.sign_up, name='sign_up'),
     path('log_in/', log_in.log_in, name='log_in'),
+    path('log_out/', log_out.log_out, name='log_out'),
     path('register_business/',
          create_business_profile.create_business_profile,
          name='create_business_profile'),
     path('register_professional/',
          create_professional_profile.create_professional_profile,
          name='create_professional_profile'),
+     path('create_job_post/', create_job_post.create_job_post,
+          name='create_job_post'),
 ]
---
+++ Harmonix/views/registers/create_job_post.py
@@ -0,0 +1,28 @@
+from ...forms.job_post_form import JobPostForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def create_job_post(request):
+    """This method creates a job post.
+    Args:
+        request: The full HTTP request object.
+    Returns:
+        A redirect to the job post page.
+    """
+    if request.method == 'POST':
+        form = JobPostForm(request.POST)
+        if form.is_valid():
+            job_post = form.process_job_post()
+            if job_post:
+                messages.success(request, 'Job Post Created successfully.')
+                return redirect('self_job_posts')
+            else:
+                messages.error(request, 'Job Post Creation failed.')
+                messages.error(request, form.errors)
+                return redirect('create_job_post')
+    else:
+        form = JobPostForm()
+    return render(request, 'registers/create_job_post.html', {'form': form})
