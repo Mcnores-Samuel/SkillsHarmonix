bd5132cc14a6a9a775518468685fc4e0bf34cb12
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-02-18 01:14:17 +0200

    auto search and filter works now

--- Harmonix/views/home_page.py
+++ Harmonix/views/home_page.py
@@ -9,18 +9,29 @@ def home_page(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
     data = JobListing.objects.all().order_by('-date_posted')
+    pos = set(JobListing.objects.values_list('position', flat=True).distinct())
+    categories = set(JobListing.objects.values_list('category', flat=True).distinct())
+    locations = set(JobListing.objects.values_list('location', flat=True).distinct())
+
     if request.method == 'POST':
         query = request.POST.get('search_query')
+        location = request.POST.get('location')
+        category = request.POST.get('category')
+        position = request.POST.get('position')
         if query:
             data = data.filter(
                 Q(title__icontains=query) |
                 Q(position__icontains=query) |
                 Q(roles__icontains=query) |
-                Q(benefits__icontains=query) |
-                Q(qualifications__icontains=query) |
                 Q(location__icontains=query) |
                 Q(category__icontains=query)
             ).distinct()
+        if location:
+            data = data.filter(location=location)
+        if category:
+            data = data.filter(category=category)
+        if position:
+            data = data.filter(position=position)
     
     paginator = Paginator(data, 12)
     page = request.GET.get('page')
@@ -32,5 +43,8 @@ def home_page(request):
         data = paginator.page(paginator.num_pages)
     context = {
         'data': data,
+        'pos': pos,
+        'categories': categories,
+        'locations': locations
     }
     return render(request, 'index.html', context)
